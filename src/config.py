abcaff40901f4e698c1696be642929bdfbba1daa
Rohan <rohanmp11@gmail.com>
2026-07-25 01:17:43 +0100

    Setup of config.py

---
+++ src/config.py
@@ -0,0 +1,7 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
+CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
+REDIRECT_URI = os.getenv("SPOTIFY_REDIRECT_URI")
