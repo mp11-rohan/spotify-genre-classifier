efb649d082beadc2243c13e72eec6db6fd62d795
Rohan <rohanmp11@gmail.com>
2026-07-30 10:47:36 +0100

    added first genre and started scoring system

---
+++ src/genres.py
@@ -0,0 +1,13 @@
+genres = {
+    "Hip Hop & Rap" : {
+        "features" : {
+            "speechiness" : [0.05, 0.55],
+            "instrumentalness" : [0.0, 0.1],
+            "danceability" : [0.60, 0.95],
+            "acousticness" : [0.0, 0.3],
+            "energy" : [0.40, 0.85]
+        },
+
+        "required" : 3
+    },
+}
---
+++ src/scoring.py
@@ -0,0 +1,3 @@
+def pass_feature(feat_val, bounds):
+    return feat_val >= bounds[0] and feat_val <= bounds[1]
+
