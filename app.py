b6db6f510a689de2b0f894b5b247ea9f59071543
Rohan <rohanmp11@gmail.com>
2026-08-26 16:14:55 +0200

    finished genre selection

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from src import spotify_client as spc
 from src import genres as gr
 import customtkinter as ctk
 
-# AFTER GUI DONE: playists = {playlist['name']:playlist['id'] for playlist in spc.user_own_playlists()}
+playists = {playlist['name']:playlist['id'] for playlist in spc.user_own_playlists()}
 
 app = ctk.CTk()
 app.geometry("700x650")
@@ -37,10 +37,10 @@ source_label = ctk.CTkLabel(
 )
 source_label.grid(row=0, column=0, sticky="ew", padx=10, pady=10)
 
-# AFTER GUI DONE: source_opts = playists
-# AFTER GUI DONE: source_opts["Liked Songs"] = "LIKED_SONGS"
+source_opts = {"Liked Songs" : "LIKED_SONGS"}
+source_opts.update(playists)
 # Set "values" to list(source_opts.keys())
-source_dropdown = ctk.CTkComboBox(master=source_frame, values=["Soft Pop", "Liked Songs"], state="readonly")
+source_dropdown = ctk.CTkComboBox(master=source_frame, values=list(source_opts.keys()), state="readonly")
 source_dropdown.grid(row=0, column=1, sticky="ew", padx=10, pady=10)
 
 #------------------------------------------Destination Config------------------------------------------------------------------
@@ -56,22 +56,37 @@ dest_label = ctk.CTkLabel(
     font=("Arial Rounded MT Bold", 18, "bold")
 )
 dest_label.grid(row=0, column=0, sticky="ew", padx=10, pady=10)
-# AFTER GUI DONE: dest_opts = playists
-# AFTER GUI DONE: dest_opts["Create New"] = None
+
 def on_selection_change(selected_name):
     if selected_name == "Create New":
         new_playlist_entry.grid(row=1, column=0,sticky="ew", columnspan=2, padx=10, pady=10)
     else:
         new_playlist_entry.grid_forget()
-dest_dropdown = ctk.CTkComboBox(master=dest_frame, values=["Hip-Hop & Rap", "Create New"], command=on_selection_change, state="readonly")
-dest_dropdown.grid(row=0, column=1, sticky="ew", padx=10, pady=10)
 new_playlist_entry = ctk.CTkEntry(master=dest_frame, placeholder_text="New playlist name")
+
+dest_opts = {"Create New" : None}
+dest_opts.update(playists)
+dest_dropdown = ctk.CTkComboBox(master=dest_frame, values=list(dest_opts.keys()), command=on_selection_change, state="readonly")
+dest_dropdown.grid(row=0, column=1, sticky="ew", padx=10, pady=10)
 #------------------------------------------Genre Config-----------------------------------------------------------------------
-# gr_label=ctk.CTkLabel(app, text="Choose a mood:")
-# gr_label.pack(padx=30, anchor="nw", side="top")
-# gr_opts = list(gr.genres.keys())
-# gr_dropdown = ctk.CTkComboBox(app, values=gr_opts, state="readonly")
-# gr_dropdown.pack(padx=30, pady=15, anchor="nw", side="top")
+gr_frame = ctk.CTkFrame(master=app, width=650, height=60)
+gr_frame.grid(row=3, column=0, sticky="ew", pady=20, padx=50)
+gr_frame.grid_columnconfigure(1, weight=1)
+gr_frame.grid_rowconfigure(0, weight=1)
+
+gr_label=ctk.CTkLabel(
+    master=gr_frame,
+    text="Choose a mood:",
+    text_color="white",
+    font=("Arial Rounded MT Bold", 18, "bold")
+    )
+gr_label.grid(row=0, column=0, sticky="ew", padx=10, pady=10)
+gr_opts = list(gr.genres.keys())
+gr_dropdown = ctk.CTkComboBox(master=gr_frame, values=gr_opts, state="readonly")
+gr_dropdown.grid(row=0, column=1, sticky="ew", padx=10, pady=10)
+
+#------------------------------------------Create Config-----------------------------------------
+
 
 
 app.mainloop()
